c7eaf88d83a2043a1ae70ecc99002c0c97478717
Reimu Li <muzi502.li@gmail.com>
2021-08-15 13:13:22 +0800

    chore(*): update download.yml version and fix image list typo

--- hack/build/generate.sh
+++ hack/build/generate.sh
@@ -9,7 +9,7 @@ REPO_PATH="${SCRIPT_PATH%/hack/build}"
 : ${DOWNLOAD_YML:="config/group_vars/all/download.yml"}
 
 # ARCH used in convert {%- if image_arch != 'amd64' -%}-{{ image_arch }}{%- endif -%} to {{arch}}
-if [[ "${IMAGE_ARCH}" != "amd64" ]]; then ARCH="${IMAGE_ARCH}"; fi
+if [[ "${IMAGE_ARCH}" != "amd64" ]]; then ARCH="-${IMAGE_ARCH}"; fi
 
 cat > /tmp/generate.sh << EOF
 arch=${ARCH}
@@ -31,6 +31,8 @@ grep '_download_url:' ${REPO_PATH}/${DOWNLOAD_YML} \
 
 # generate download images list
 grep -E '_image_tag:|_image_repo:|_image_name:' ${REPO_PATH}/${DOWNLOAD_YML} \
+| sed "s#{%- if image_arch != 'amd64' -%}-{{ image_arch }}{%- endif -%}#{{arch}}#g" \
 | sed 's/: /=/g;s/{{/${/g;s/}}/}/g' | tr -d ' ' >> /tmp/generate.sh
+
 grep '_image_name:' ${REPO_PATH}/${DOWNLOAD_YML} \
 | cut -d ':' -f1 | sed 's/^/echo $/g' >> /tmp/generate.sh
