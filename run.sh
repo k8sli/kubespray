2ad1aa68dc8424a56646b7df91c7ab40fab1bbff
Reimu Li <muzi502.li@gmail.com>
2021-08-21 12:18:03 +0800

    chore(*): fix offline resource repo config typo

--- run.sh
+++ run.sh
@@ -11,41 +11,29 @@ KUBE_ROOT="$(cd "$(dirname "$0")" && pwd)"
 : ${ENV_FILE:=${KUBE_ROOT}/config/env.yml}
 : ${INSTALL_STEPS_FILE:=${KUBE_ROOT}/config/.install_steps}
 
+ANSIBLE_ARGS="-f ${ANSIBLE_FORKS} --become --become-user=root -i ${INVENTORY} -e @${ENV_FILE}"
+
 #
-# Set Colors
+# Set logging colors
 #
 
 NORMAL_COL=$(tput sgr0)
 RED_COL=$(tput setaf 1)
-GREEN_COL=$(tput setaf 76)
 WHITE_COL=$(tput setaf 7)
+GREEN_COL=$(tput setaf 76)
 YELLOW_COL=$(tput setaf 202)
 
-#
-# Headers and Logging
-#
-
-debuglog(){
-  printf "${WHITE_COL}%s${NORMAL_COL}\n" "$@"
-}
-
-infolog(){
-  printf "${WHITE_COL}➜ %s${NORMAL_COL}\n" "$@"
-}
-
-success(){
-  printf "${GREEN_COL}✔ %s${NORMAL_COL}\n" "$@"
-}
-
-warnlog(){
-  printf "${YELLOW_COL}➜ %s${NORMAL_COL}\n" "$@"
-}
-
-errorlog(){
-  printf "${RED_COL}✖ %s${NORMAL_COL}\n" "$@"
-}
+debuglog(){ printf "${WHITE_COL}%s${NORMAL_COL}\n" "$@"; }
+infolog(){ printf "${GREEN_COL}✔ %s${NORMAL_COL}\n" "$@"; }
+warnlog(){ printf "${YELLOW_COL}➜ %s${NORMAL_COL}\n" "$@"; }
+errorlog(){ printf "${RED_COL}✖ %s${NORMAL_COL}\n" "$@"; }
 
 set -eo pipefail
+
+if [[ ! -f ${INVENTORY} ]];then
+  errorlog "${INVENTORY} file is missing, please check the inventory file is exists"
+  exit 1
+fi
 
 deploy_cluster(){
   touch ${INSTALL_STEPS_FILE}
@@ -53,7 +41,7 @@ deploy_cluster(){
   for step in ${STEPS}; do
     if ! grep -q "${step}" ${INSTALL_STEPS_FILE}; then
       infolog "######  start deploy ${step}  ######"
-      if ansible-playbook -f ${ANSIBLE_FORKS} --become --become-user=root -i ${INVENTORY} -e "@${ENV_FILE}" ${KUBE_ROOT}/playbooks/${step}.yml; then
+      if ansible-playbook ${ANSIBLE_ARGS} ${KUBE_ROOT}/playbooks/${step}.yml; then
         echo ${step} >> ${INSTALL_STEPS_FILE}
         infolog "######  ${step} successfully installed  ######"
       else
@@ -61,7 +49,7 @@ deploy_cluster(){
         exit 1
       fi
     else
-      infolog "######  ${step} is already installed, so skipped...  ######"
+      warnlog "######  ${step} is already installed, so skipped...  ######"
     fi
   done
 }
@@ -71,22 +59,24 @@ main(){
     deploy-cluster)
       infolog "######  start deploy kubernetes cluster  ######"
       deploy_cluster
+      infolog "######  kubernetes cluster successfully installed  ######"
       ;;
     remove-cluster)
       infolog "######  start remove kubernetes cluster  ######"
-      if ansible-playbook -f ${ANSIBLE_FORKS} --become --become-user=root -i ${INVENTORY} -e "@${ENV_FILE}" ${KUBE_ROOT}/reset.yml >/dev/stdout 2>/dev/stderr; then
+      if ansible-playbook ${ANSIBLE_ARGS} ${KUBE_ROOT}/reset.yml >/dev/stdout 2>/dev/stderr; then
         rm -f ${INSTALL_STEP_FILE}
+        infolog "######  kubernetes cluster successfully removed ######"
       fi
       ;;
     add-node)
       check_nodename_exist
       infolog "######  start add worker to kubernetes cluster  ######"
-      ansible-playbook -f ${ANSIBLE_FORKS} -i ${INVENTORY} --become --become-user=root -e "@${ENV_FILE}" --limit="$NODES_NAME" ${KUBE_ROOT}/playbooks/10-scale-nodes.yml >/dev/stdout 2>/dev/stderr
+      ansible-playbook ${ANSIBLE_ARGS} --limit="$NODES_NAME" ${KUBE_ROOT}/playbooks/10-scale-nodes.yml >/dev/stdout 2>/dev/stderr
       ;;
     remove-node)
       check_nodename_exist
       infolog "######  start remove worker from kubernetes cluster  ######"
-      ansible-playbook -f ${ANSIBLE_FORKS} -i ${INVENTORY} --become --become-user=root -e "@${ENV_FILE}" -e node="$NODES_NAME" -e reset_nodes=true ${KUBE_ROOT}/remove-node.yml >/dev/stdout 2>/dev/stderr
+      ansible-playbook ${ANSIBLE_ARGS} -e node="$NODES_NAME" -e reset_nodes=true ${KUBE_ROOT}/remove-node.yml >/dev/stdout 2>/dev/stderr
       ;;
     *)
       errorlog "unknow [TYPE] parameter: ${TYPE}"
